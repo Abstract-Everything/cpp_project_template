3eb81e2eef425b4041dc7fdb40e843d0d52e1103
Jonathan Camilleri <jon.d.cam@gmail.com>
2021-06-20 19:22:00 +0200

    [cmake] Create separete file for precompiled header defaults

---
+++ cmake/PrecompileHeaders.cmake
@@ -0,0 +1,17 @@
+function(enable_pch)
+	# Very basic PCH example
+	option(ENABLE_PCH "Enable Precompiled Headers" OFF)
+	if(ENABLE_PCH)
+		# This sets a global PCH parameter, each project will build its own PCH,
+		# which is a good idea if any #define's change
+		#
+		# consider breaking this out per project as necessary
+		target_precompile_headers(
+			project_options
+			INTERFACE
+			<vector>
+			<string>
+			<map>
+			<utility>)
+	endif()
+endfunction()
