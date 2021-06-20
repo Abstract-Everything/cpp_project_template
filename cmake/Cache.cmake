568b82c7c86f625a0c8e6d354148732f02cba51b
Jonathan Camilleri <jon.d.cam@gmail.com>
2021-06-20 19:11:46 +0200

    [cmake] Use consistent style for includes
    All options in external files should be in a funciton named enable_x
    which is called from the main CMakeLists.txt.

--- cmake/Cache.cmake
+++ cmake/Cache.cmake
@@ -1,22 +1,24 @@
-set(CACHE_OPTION
-    "ccache"
-    CACHE STRING "Compiler cache to be used")
-set_property(
-        CACHE CACHE_OPTION
-        PROPERTY STRINGS
-                 "off"
-                 "ccache"
-                 "sccache")
+function(enable_ccache)
+	set(CACHE_OPTION
+	    "ccache"
+	    CACHE STRING "Compiler cache to be used")
+	set_property(
+		CACHE CACHE_OPTION
+		PROPERTY STRINGS
+			 "off"
+			 "ccache"
+			 "sccache")
 
-if(CACHE_OPTION STREQUAL "off")
-        return()
-endif()
+	if(CACHE_OPTION STREQUAL "off")
+		return()
+	endif()
 
-find_program(CACHE_BINARY ${CACHE_OPTION})
-if(NOT CACHE_BINARY)
-        message(FATAL_ERROR "${CACHE_OPTION} is enabled but was not found")
-        return()
-endif()
+	find_program(CACHE_BINARY ${CACHE_OPTION})
+	if(NOT CACHE_BINARY)
+		message(FATAL_ERROR "${CACHE_OPTION} is enabled but was not found")
+		return()
+	endif()
 
-message(STATUS "${CACHE_OPTION} found and enabled")
-set(CMAKE_CXX_COMPILER_LAUNCHER ${CACHE_BINARY})
+	message(STATUS "${CACHE_OPTION} found and enabled")
+	set(CMAKE_CXX_COMPILER_LAUNCHER ${CACHE_BINARY})
+endfunction()
--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -4,9 +4,11 @@ if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
                 STATUS
                         "Setting build type to 'RelWithDebInfo' as none was specified."
         )
+
         set(CMAKE_BUILD_TYPE
             RelWithDebInfo
             CACHE STRING "Choose the type of build." FORCE)
+
         # Set the possible values of build type for cmake-gui, ccmake
         set_property(
                 CACHE CMAKE_BUILD_TYPE
@@ -21,22 +23,9 @@ endif()
 # tools
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-option(ENABLE_IPO
-       "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)"
-       OFF)
-
-if(ENABLE_IPO)
-        include(CheckIPOSupported)
-        check_ipo_supported(RESULT result OUTPUT output)
-        if(result)
-                set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
-        else()
-                message(SEND_ERROR "IPO is not supported: ${output}")
-        endif()
-endif()
-if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+if(${CPP_PROJECT_TEMPLATE_USING_CLANG})
         add_compile_options(-fcolor-diagnostics)
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+elseif(${CPP_PROJECT_TEMPLATE_USING_GCC})
         add_compile_options(-fdiagnostics-color=always)
 else()
         message(
