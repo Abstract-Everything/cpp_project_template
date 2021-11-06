07359d97d5617f831fef9b5b8dfad080fec87915
Jonathan Camilleri <jon.d.cam@gmail.com>
2021-11-06 13:58:27 +0100

    [cmake] Do not download unnecessary conan packages

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,9 @@
 #include <iostream>
 
 #include <spdlog/spdlog.h>
-#include <docopt/docopt.h>
 
 static constexpr auto USAGE =
-  R"(Naval Fate.
+    R"(Naval Fate.
 
     Usage:
           naval_fate ship new <name>...
@@ -14,7 +13,8 @@ static constexpr auto USAGE =
           naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
           naval_fate (-h | --help)
           naval_fate --version
- Options:
+
+    Options:
           -h --help     Show this screen.
           --version     Show version.
           --speed=<kn>  Speed in knots [default: 10].
@@ -22,20 +22,11 @@ static constexpr auto USAGE =
           --drifting    Drifting mine.
 )";
 
-int main(int argc, const char **argv)
+int main(int /* argc */, const char ** /* argv */)
 {
-  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
-    { std::next(argv), std::next(argv, argc) },
-    true,// show help if requested
-    "Naval Fate 2.0");// version string
+	std::cout << USAGE;
 
-  for (auto const &arg : args) {
-    std::cout << arg.first << arg.second << std::endl;
-  }
+	spdlog::info("Hello, {}!", "World");
 
-
-  //Use the default logger (stdout, multi-threaded, colored)
-  spdlog::info("Hello, {}!", "World");
-
-  fmt::print("Hello, from {}\n", "{fmt}");
+	fmt::print("Hello, from {}\n", "{fmt}");
 }
